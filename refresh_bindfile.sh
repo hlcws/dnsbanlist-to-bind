5216d5b530f565f328b0305f3cbab6805032efb0
hlcws <hlcws@users.noreply.github.com>
2019-08-22 18:23:59 +0200

    Update refresh_bindfile.sh

--- refresh_bindfile.sh
+++ refresh_bindfile.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+banlisturl="https://raw.githubusercontent.com/hectorm/hmirror/master/data/stevenblack/list.txt"
+templist="/etc/bind/banlist.txt"
+zonefile="/etc/bind/named.conf.banned"
+/usr/bin/wget -O $templist $banlisturl
 prefix="zone \""
 suffix="\"{ type master; notify no; file \"/etc/bind/db.empty\"; };"
-echo $prefix
-echo $suffix
-sed "s,.*,$prefix&$suffix," /etc/bind/banlist.txt > /etc/bind/named.conf.banned
+sed "s,.*,$prefix&$suffix," $templist > $zonefile
+/usr/sbin/service bind9 reload
+/usr/sbin/service bind9 status
